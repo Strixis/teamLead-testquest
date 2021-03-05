45c3a9974395787749bf78bcce71fa579e2836c2
Strixis <nivem-strixis@mail.ru>
2021-03-05 12:29:49 +0300

    Сделан плавный скролл до формы с заказом

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,35 @@
+import { Carousel } from './carousel';
+import { Timer } from './timer';
+import { Scroll } from './scrolling';
+
+const carousel = new Carousel({
+  carouselClass: 'carousel',
+  currentSlideClass: 'carousel_slide__current',
+  prevSlideClass: 'carousel_slide__prev',
+  nextSlideClass: 'carousel_slide__next',
+  controlsClass: 'carousel_controls',
+  controlLeftClass: 'carousel_left-control',
+  controlRightClass: 'carousel_right-control',
+});
+
+const timer = new Timer({
+  time: {
+    hours: 0,
+    minutes: 2,
+    seconds: 10,
+  },
+  timerClass: 'timer',
+  hoursClass: 'hours',
+  minutesClass: 'minutes',
+  secondsClass: 'seconds',
+})
+
+const scroll = new Scroll({
+  linkClass: 'button-order',
+  linkTag: 'a',
+  targetId: 'order-form',
+});
+
+carousel.run();
+timer.run();
+scroll.run();
--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
 import 'fonts';
 import 'styles';
-
-import { Carousel } from 'scripts/carousel';
-import { Timer } from 'scripts/timer'
-
-const carousel = new Carousel({
-  carouselClass: 'carousel',
-  currentSlideClass: 'carousel_slide__current',
-  prevSlideClass: 'carousel_slide__prev',
-  nextSlideClass: 'carousel_slide__next',
-  controlsClass: 'carousel_controls',
-  controlLeftClass: 'carousel_left-control',
-  controlRightClass: 'carousel_right-control',
-});
-
-const timer = new Timer({
-  time: {
-    hours: 0,
-    minutes: 2,
-    seconds: 10,
-  },
-  timerClass: 'timer',
-  hoursClass: 'hours',
-  minutesClass: 'minutes',
-  secondsClass: 'seconds',
-})
-
-carousel.run();
-timer.run();
+import 'scripts';
